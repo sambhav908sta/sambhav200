90f4c8964320e958740e0f5252e06ea7dd28ee68
sambhav908sta <sambhav908sta@gmail.com>
2020-11-15 16:37:10 +0530

    initial commit

---
+++ polls/admin.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+#from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
